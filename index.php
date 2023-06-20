60811a45663139f60b06b7da9e437930ccc5d162
Mateo O <orehurcomito@gmail.com>
2023-06-19 22:00:36 -0500

    Update docs

--- index.php
+++ index.php
@@ -5,7 +5,10 @@ use Mateodioev\HttpRouter\{Request, Response, Router};
 
 require __DIR__ . '/vendor/autoload.php';
 
-$router = new Router();
+$conf = new \Mateodioev\StringVars\Config;
+$conf->addFormat('uuid', '([0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12})');
+
+$router = new Router($conf);
 
 $router->get('/', function () {
     return Response::text('Hello world!')
@@ -26,6 +29,12 @@ $router->get('/page/{all:id}?', function (Request $r) {
 $router->get('/usr/{name}', function (Request $r) {
     return Response::text('Hello ' . $r->param('name'));
 });
+
+// Using custom format
+$router->get('/user/{uuid:id}', function (Request $r) {
+    return Response::text('Hello user id ' . $r->param('id'));
+});
+// try: /usr/123e4567-e89b-12d3-a456-426655440000
 
 try {
     $router->run();
